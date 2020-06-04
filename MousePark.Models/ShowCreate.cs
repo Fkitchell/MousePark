d428e4179c32f34d8cc5f3e45e97b489b420a9fc
Sabra Snider <sabralsnider@gmail.com>
2020-06-04 10:42:04 -0400

    Built Show Models for Create, Detail, Edit, ListItem

---
+++ MousePark.Models/ShowCreate.cs
@@ -0,0 +1,21 @@
+﻿using MousePark.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePark.Models
+{
+    public class ShowCreate
+    {
+        [Required]
+        public string ShowName { get; set; }
+        public TargetAge TargetAge { get; set; }
+        public int Capactiy { get; set; }
+        public int RunTime { get; set; }
+        [Required]
+        public int AreaId { get; set; }
+    }
+}
---
+++ MousePark.Models/ShowEdit.cs
@@ -0,0 +1,19 @@
+﻿using MousePark.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePark.Models
+{
+    public class ShowEdit
+    {
+        public int ShowId { get; set; }
+        public string ShowName { get; set; }
+        public TargetAge TargetAge { get; set; }
+        public int Capacity { get; set; }
+        public int RunTime { get; set; }
+        public int AreaId { get; set; }
+    }
+}
