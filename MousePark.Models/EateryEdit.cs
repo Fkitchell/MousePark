5af8b586fcefcc2c2c913f750cb95c255b47c68e
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-04 10:02:51 -0400

    Added Eatery class, models, service and controller

---
+++ MousePark.Models/EateryEdit.cs
@@ -0,0 +1,29 @@
+﻿using MousePark.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePark.Models
+{
+   public class EateryEdit
+    {
+        public int EateryId { get; set; }
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        [Description("Name")]
+        public string EateryName { get; set; }
+
+        [Description("Cuisine Type")]
+        public string CuisineType { get; set; }
+
+        [Description("Dine-In Y / N")]
+        public bool DineIn { get; set; }
+
+        [Description("Price Tier")]
+        public PriceTier Tier { get; set; }
+    }
+}
---
+++ MousePark.Services/EateryService.cs
@@ -0,0 +1,84 @@
+﻿using MousePark.Data;
+using MousePark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePark.Services
+{
+    public class EateryService
+    {
+       
+        public bool CreateEatery(EateryCreate model)
+        {
+            var food =
+                new Eatery()
+                {
+                    EateryName = model.EateryName
+                };
+            using (var fd = new ApplicationDbContext())
+            {
+                fd.Eateries.Add(food);
+                return fd.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<EateryListItem> GetEateries()
+        {
+            using (var fd = new ApplicationDbContext())
+            {
+                var query =
+                   fd.Eateries
+                   .Select(
+                       f =>
+                   new EateryListItem
+                   {
+                       EateryId = f.EateryId,
+                       EateryName = f.EateryName,
+                   }
+                   );
+                return query.ToArray();
+            }
+        }
+        public EateryDetail GetEateryById(int areaId)
+        {
+            using (var fd = new ApplicationDbContext())
+            {
+                var food =
+                    fd.Eateries.Single(f => f.EateryId == areaId);
+                return
+                    new EateryDetail
+                    {
+                        EateryId = food.EateryId,
+                        EateryName = food.EateryName,
+                    };
+            }
+        }
+        public bool UpdateEatery(EateryEdit model)
+        {
+            using (var fd = new ApplicationDbContext())
+            {
+                var food =
+                    fd
+                    .Eateries
+                    .Single(f => f.EateryId == model.EateryId);
+                food.EateryName = model.EateryName;
+
+                return fd.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteEatery(int EateryId)
+        {
+            using (var fd = new ApplicationDbContext())
+            {
+                var food =
+                    fd
+                    .Eateries
+                    .Single(f => f.EateryId == EateryId);
+                fd.Eateries.Remove(food);
+                return fd.SaveChanges() == 1;
+            }
+        }
+    }
+}
